347d3530ec47dd214527aa982f523333d69f8f77
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-08-13 00:55:15 -0500

    feat:Creacion de clase Alien con 8 atributos privados

--- src/com/krakedev/alien/Aliens.java
+++ src/com/krakedev/alien/Aliens.java
@@ -1,5 +1,14 @@
 package com.krakedev.alien;
 
 public class Aliens {
+	/*<<Atributos privados >>*/
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidades;
+	private double precioOjos;
+	private double precioCuerpo;
 
 }

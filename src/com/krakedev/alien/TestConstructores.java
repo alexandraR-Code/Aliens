41607d1fe6c0ae38226951a50e48e7c15ec09889
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-08-14 20:20:02 -0500

    test: agregar clase TestConstructor con metodo main, creacion de 3 objetos con un llamado a metodo imprimir

---
+++ src/com/krakedev/alien/TestConstructores.java
@@ -0,0 +1,19 @@
+package com.krakedev.alien;
+
+public class TestConstructores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("==========================================");
+		Aliens aliens1 = new Aliens(10, "verde");
+		aliens1.imprimir();
+		System.out.println("==========================================");
+		Aliens aliens2 = new Aliens(4, "rosa");
+		aliens2.imprimir();
+		System.out.println("==========================================");
+		Aliens aliens3 = new Aliens(45, "Azul");
+		aliens3.imprimir();
+
+	}
+
+}
